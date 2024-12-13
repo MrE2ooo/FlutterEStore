c0308b7d3b124761225e9a30a3156309512d953c
MrE2000 <eliasymadani@gmail.com>
2024-12-13 12:35:43 +0300

    dartz_Either_errorhandling

--- lib/features/Auth/data/respotries/auth_repo_imp.dart
+++ lib/features/Auth/data/respotries/auth_repo_imp.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:estore/features/Auth/domain/respotries/auth_repo.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -5,37 +6,33 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthRepoImp extends AuthRepo {
   @override
-  Future CompleteInformation({String? name, String? phoneNumber, String? address}) {
+  Future CompleteInformation(
+      {String? name, String? phoneNumber, String? address}) {
     // TODO: implement CompleteInformation
     throw UnimplementedError();
   }
 
-
   @override
   // ignore: non_constant_identifier_names
-  Future LoginWithGoogle()async {
-    
+  Future<Either<Exception, UserCredential>> LoginWithGoogle() async {
+    // Trigger the authentication flow
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
 
-  // Trigger the authentication flow
-  try {
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-  
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-  
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-  
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-} on Exception catch (e) {
-  
-}
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
 
-  
-
+      return right(
+          await FirebaseAuth.instance.signInWithCredential(credential));
+    } on Exception catch (e) {
+       left(throw Exception('Failed to sign in with Google: $e'));
+    }
   }
 }
--- lib/features/Auth/domain/respotries/auth_repo.dart
+++ lib/features/Auth/domain/respotries/auth_repo.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 abstract class AuthRepo {
   // ignore: non_constant_identifier_names
-  Future LoginWithGoogle();
+  Future<Either<Exception,UserCredential>> LoginWithGoogle();
  
   // ignore: non_constant_identifier_names
   Future CompleteInformation({@required String name,@required String phoneNumber,@required String address});
